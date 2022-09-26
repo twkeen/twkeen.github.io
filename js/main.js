9ab682f43ca3a94db9797fb8ffa695579b76203f
Your Name <you@example.com>
2022-09-25 21:05:40 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -5,31 +5,39 @@ $(document).ready(function() {
 
   // Home
   $("body").on("click", "#a-home, #a-home-logo", function() {
-  //$("#a-home, #a-home-logo" ).click(function() {
     $("#main").load("page/home.html");
   }); 
   
    // Welcome Result
    $("body").on("click", "#a-welcome", function() {
-   //$("#a-welcome").click(function() {
     $("#main").load("page/welcome.html");
   }); 
 
    // Competition Result
    $("body").on("click", "#a-news", function() {
-   //$("#a-news").click(function() {
     $("#main").load("page/news.html");
   }); 
   
   // Competition Result
   $("body").on("click", "#a-competition-result", function() {
-  //$("#a-competition-result").click(function() {
     $("#main").load("page/competeResult.html");
+    // build date links
+    $.get('./data/competeDate.txt', function (data) {
+      let lines = data.split("\n");
+      $.each(lines, function (n, line) {
+        let dateLink = '<div class="row">' +
+                  '  <div class="col-xs-12">' +
+                  '    <a target="_blank" href="./file/competeResult/' + line + '.pdf">' + line + '</a>' +
+                  '  </div>' +
+                  '</div>';
+        $('#div-dates').append(dateLink);
+      });
+    });
+  
   });
 
   // Past AMC problems and Solutions
   $("body").on("click", "#a-amc", function() {
-    //$("#a-competition-result").click(function() {
       $("#main").load("page/amc.html");
     });
 });
